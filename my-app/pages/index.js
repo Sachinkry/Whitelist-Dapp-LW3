3133a1c361142df8c162a1efbddde39687395182
Sachin <heysachinkry@gmail.com>
2022-07-10 19:25:32 +0530

    connected wallet using web3modal

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,20 +1,49 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-// import Web3Modal from "web3modal";
-// import { providers, Contract } from "ethers"
-import { useRef, useState } from "react";
-// import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
+import Web3Modal from "web3modal";
+import { useRef, useEffect, useState } from "react";
+import { providers } from "ethers";
+
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef() // return the object with key named current
 
-  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+  useEffect(() => {
+    web3ModalRef.current = new Web3Modal({
+      network: "rinkeby",
+      providerOptions: {},
+    });
+    console.log("FDSSDF", web3ModalRef.current);
+  }, [walletConnected]);
 
-  const [loading, setLoading] = useState(false);
+  const getSignerOrProvider = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      alert('SWITCH TO RINKEBY NETWORK');
+      throw new Error('Change to rinkeby');
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
 
-  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0);
 
-  const web3ModalRef = useRef();
+
+
+  async function connectWallet() {
+    try {
+      await getSignerOrProvider();
+      setWalletConnected(true);
+      console.log("maniac")
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
 
   return (
     <div>
@@ -23,26 +52,25 @@ export default function Home() {
         <meta name="description" content="Whitelist-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className={styles.title}>
+      <div className={styles.main}>
         <div>
-          <h1 className={styles.title} >Welcome to Crypto Devs</h1>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
           <div className={styles.description}>
-            It's an NFT collection for developers in Crypto.
+            Its an NFT collection for developers in Crypto.
           </div>
           <div className={styles.description}>
-            {numberOfWhitelisted} have already joined the Whitelist
+            0 have already joined the Whitelist
           </div>
-          {/* {renderButton()} */}
+          <button className={styles.btn} onClick={connectWallet}>{!walletConnected ? 'Connect Wallet' : 'Connected'}</button>
         </div>
-        <div >
-          <img className={styles.images} src="./crypto-devs.svg" />
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
         </div>
-
       </div>
+
       <footer className={styles.footer}>
         Made with &#10084; by Crypto Devs
       </footer>
     </div>
-  )
+  );
 }
