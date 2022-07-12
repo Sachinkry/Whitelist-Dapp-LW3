b614ac8d8dfe37fd9624a8eef5bb185faa1ac171
Sachin <heysachinkry@gmail.com>
2022-07-12 19:06:48 +0530

    messed up :-(

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,26 +1,27 @@
-const { ethers } = require("hardhat");
+const { ethers } = require('hardhat');
 
 async function main() {
 
+    // it looks for abi code and bytecode in the artifacts directory and store it in whitelistContract
     const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-    // here we deploy the contract
+    //here we deploy the contract
     const deployedWhitelistContract = await whitelistContract.deploy(10);
+    // 10 is the Maximum number of whitelisted addresses allowed
 
     // wait for it to finish deploying
     await deployedWhitelistContract.deployed();
 
-    // print the address of the deployed contract
     console.log(
         "Whitelist Contract Address:",
         deployedWhitelistContract.address
     );
 }
 
-// Call the main function and catch if there is any error
+// call the main function and catch if there is an error
 main()
     .then(() => process.exit(0))
     .catch((error) => {
         console.error(error);
-        process.exit(1)
-    })
+        process.exit(1);
+    });
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,46 +2,107 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { useRef, useEffect, useState } from "react";
-import { providers } from "ethers";
+import { providers, Contract } from "ethers";
+import {WHITELIST_CONTRACT_ADDRESS, abi} from "../constants"
 
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
-  const web3ModalRef = useRef() // return the object with key named current
+  const [joinedWhitelist, setJoinedWhitelist] = useState(false);
+  const [numberOfWhitelisted, setNumberOfWhitelisted] = useState(0)
+  const [loading, setLoading] = useState(false);
+  const web3ModalRef = useRef();
 
   useEffect(() => {
-    web3ModalRef.current = new Web3Modal({
-      network: "rinkeby",
-      providerOptions: {},
-    });
-    console.log("FDSSDF", web3ModalRef.current);
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {}
+      });
+    }
+    console.log(web3ModalRef.current);
   }, [walletConnected]);
 
   const getSignerOrProvider = async (needSigner = false) => {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
+    // console.log(provider);
+    console.log(web3Provider);
     const { chainId } = await web3Provider.getNetwork();
+
     if (chainId !== 4) {
-      alert('SWITCH TO RINKEBY NETWORK');
-      throw new Error('Change to rinkeby');
+      alert("NOT ON RINKEBY NETWORK");
+      throw error("change to rinkeby network");
     }
+
     if (needSigner) {
       const signer = web3Provider.getSigner();
       return signer;
     }
     return web3Provider;
-  };
+  }
 
+  const addAddressToWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const tx = await whitelistContract.addAddressToWhitelist();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
 
+      await getNumberOfWhitelisted();
+      setJoinedWhitelist(true);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
+  const getNumberOfWhitelisted = async () => {
+    try {
+      const provider = await getSignerOrProvider();
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      const _numberOfWhitelisted = await whitelistContract.numAddressesWhitelisted();
+      setNumberOfWhitelisted(_numberOfWhitelisted);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
-  async function connectWallet() {
+  const checkIfAddressInWhitelist = async () => {
+    try {
+      const signer = await getSignerOrProvider(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const address = await signer.getAddress();
+      const _joinedWhitelist = await WHITELIST_CONTRACT_ADDRESS.whitelistedAddresses(address);
+      setJoinedWhitelist(_joinedWhitelist)
+    } catch (err) {
+      console.err
+    }
+  }
+
+  const connectWallet = async () => {
     try {
       await getSignerOrProvider();
       setWalletConnected(true);
-      console.log("maniac")
-    } catch (error) {
-      console.log("error", error);
+
+      checkIfAddressInWhitelist();
+      getNumberOfWhitelisted();
+
+    } catch (err) {
+      console.log('error:', err);
     }
   }
 
@@ -59,9 +120,9 @@ export default function Home() {
             Its an NFT collection for developers in Crypto.
           </div>
           <div className={styles.description}>
-            0 have already joined the Whitelist
+            {numberOfWhitelisted} have already joined the Whitelist
           </div>
-          <button className={styles.btn} onClick={connectWallet}>{!walletConnected ? 'Connect Wallet' : 'Connected'}</button>
+          <button className={styles.btn} onClick={connectWallet}>Join Whitelist</button>
         </div>
         <div>
           <img className={styles.image} src="./crypto-devs.svg" />
@@ -74,3 +135,39 @@ export default function Home() {
     </div>
   );
 }
+
+// const [walletConnected, setWalletConnected] = useState(false);
+// const web3ModalRef = useRef() // return the object with key named current
+
+// useEffect(() => {
+//   web3ModalRef.current = new Web3Modal({
+//     network: "rinkeby",
+//     providerOptions: {},
+//   });
+//   console.log("FDSSDF", web3ModalRef.current);
+// }, [walletConnected]);
+
+// const getSignerOrProvider = async (needSigner = false) => {
+//   const provider = await web3ModalRef.current.connect();
+//   const web3Provider = new providers.Web3Provider(provider);
+//   const { chainId } = await web3Provider.getNetwork();
+//   if (chainId !== 4) {
+//     alert('SWITCH TO RINKEBY NETWORK');
+//     throw new Error('Change to rinkeby');
+//   }
+//   if (needSigner) {
+//     const signer = web3Provider.getSigner();
+//     return signer;
+//   }
+//   return web3Provider;
+// };
+
+// async function connectWallet() {
+//   try {
+//     await getSignerOrProvider();
+//     setWalletConnected(true);
+//     console.log("maniac")
+//   } catch (error) {
+//     console.log("error", error);
+//   }
+// }
